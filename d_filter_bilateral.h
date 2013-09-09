a65c00a576466023b937f36813ef394367a324e5
moddy3d <moddy3d@gmail.com>
2013-09-09 17:34:37 +0800

    Separated bilateral filter from filter

---
+++ d_filter_bilateral.h
@@ -0,0 +1,20 @@
+#ifndef D_FILTER_BILATERAL_H
+#define D_FILTER_BILATERAL_H
+#include <stdio.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <cuda_runtime_api.h>
+
+__global__ void filter_bilateral_1_kernel(float *img_out, float *img_in, float* kernel,
+                                   int radius, float sigma_color, float sigma_spatial,
+                                   int num_rows, int num_cols);
+
+void d_filter_bilateral_1(float *d_img,
+                          int radius, float sigma_color, float sigma_spatial,
+                          int num_rows, int num_cols);
+
+void filter_bilateral_1(float *img,
+                        int radius, float sigma_color, float sigma_spatial,
+                        int num_rows, int num_cols);
+
+#endif
